c9baaab82bd72cf29461605cd6d298f146191503
Xandra Campo <xkmpera@gmail.com>
2025-01-25 11:50:30 +0100

    Compute live time and total counts value and uncertainty

--- dev/parse_cvs_type_1.py
+++ dev/parse_cvs_type_1.py
@@ -40,6 +40,13 @@ for col in df.columns[:-1]:
 
 df[df.columns[-1]] = pd.to_datetime(df[df.columns[-1]], errors='coerce')
 
+# Compute live time
+df["LTime"]=df["Time"]-df["DTime"]
+# Compute total counts: rate count (1/min) * real time (s) / 60 (s/min)
+df["Counts_"]=df["CPM"]*df["Time"]/60
+# Compute uncertainty of total counts
+df["UCounts"]=df["Counts_"].pow(1/2)
+
 # Save the DataFrame to a CSV file
 df.to_csv('output.csv', index=False)
 
