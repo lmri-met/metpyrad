a492340fc16c9c452c3c355c76c3cc2ce3e83544
Xandra Campo <xkmpera@gmail.com>
2025-01-28 16:16:12 +0100

    Add test for result compilation and refactor tests to fix issue with reading dataframes from string CSVs

--- tests/test_hidex_tdcr.py
+++ tests/test_hidex_tdcr.py
@@ -5,6 +5,52 @@ import pytest
 
 from metpyrad.hidex_tdcr import HidexTDCRProcessor
 
+summary = """
+Cycle,Repetitions,Real time (s),Date
+1,2,100,2023-11-30 08:44:20
+2,2,100,2023-12-01 12:46:16
+"""
+readings = """
+Cycle,Sample,Repetitions,Count rate (cpm),Counts (reading),Dead time,Real time (s),End time
+1,1,1,83.97,140,1.0,100,2023-11-30 08:44:20
+1,2,1,252623.23,374237,1.125,100,2023-11-30 08:47:44
+1,1,2,87.57,146,1.0,100,2023-11-30 08:51:04
+1,2,2,251953.09,373593,1.124,100,2023-11-30 08:54:28
+2,1,1,97.77,163,1.0,100,2023-12-01 12:46:16
+2,2,1,223744.1,335987,1.11,100,2023-12-01 12:49:40
+2,1,2,85.17,142,1.0,100,2023-12-01 12:53:00
+2,2,2,223689.4,335843,1.11,100,2023-12-01 12:56:24
+"""
+background = """
+Cycle,Sample,Repetitions,Count rate (cpm),Counts (reading),Dead time,Real time (s),End time,Live time (s),Counts,Counts uncertainty,Counts uncertainty (%)
+1,1,1,83.97,140,1.0,100,2023-11-30 08:44:20,100.0,139.95,11.830046491878212,8.453052155682895
+1,1,2,87.57,146,1.0,100,2023-11-30 08:51:04,100.0,145.95,12.080976781701056,8.277476383488219
+2,1,1,97.77,163,1.0,100,2023-12-01 12:46:16,100.0,162.95,12.765187033490735,7.833806096036044
+2,1,2,85.17,142,1.0,100,2023-12-01 12:53:00,100.0,141.95,11.914277149705725,8.393291405217138
+"""
+sample = """
+Cycle,Sample,Repetitions,Count rate (cpm),Counts (reading),Dead time,Real time (s),End time,Live time (s),Counts,Counts uncertainty,Counts uncertainty (%)
+1,2,1,252623.23,374237,1.125,100,2023-11-30 08:47:44,88.88888888888889,374256.63703703706,611.7651812885701,0.16346141143464313
+1,2,2,251953.09,373593,1.124,100,2023-11-30 08:54:28,88.9679715302491,373595.9223013048,611.2249359289139,0.1636058906006906
+2,2,1,223744.1,335987,1.11,100,2023-12-01 12:49:40,90.09009009009009,335952.1021021021,579.6137525129145,0.1725286875379512
+2,2,2,223689.4,335843,1.11,100,2023-12-01 12:56:24,90.09009009009009,335869.96996996994,579.5428974372561,0.17254978094322423
+"""
+net = """
+Elapsed time,Elapsed time (s),Count rate (cpm),Counts,Counts uncertainty,Counts uncertainty (%)
+0 days 00:00:00,0.0,252539.26,374116.68703703705,611.8795527201714,0.16355313032577887
+0 days 00:06:44,404.0,251865.52,373449.9723013048,611.3443156694146,0.16370179703110896
+1 days 04:01:56,100916.0,223646.33000000002,335789.15210210206,579.7543032199951,0.17265426818901866
+1 days 04:08:40,101320.0,223604.22999999998,335728.01996996993,579.6653517073191,0.17265921139354673
+"""
+compilation = """
+Background,Background,Background,Background,Background,Background,Background,Background,Background,Background,Background,Background,Sample,Sample,Sample,Sample,Sample,Sample,Sample,Sample,Sample,Sample,Sample,Sample,Net,Net,Net,Net,Net,Net
+Cycle,Sample,Repetitions,Count rate (cpm),Counts (reading),Dead time,Real time (s),End time,Live time (s),Counts,Counts uncertainty,Counts uncertainty (%),Cycle,Sample,Repetitions,Count rate (cpm),Counts (reading),Dead time,Real time (s),End time,Live time (s),Counts,Counts uncertainty,Counts uncertainty (%),Elapsed time,Elapsed time (s),Count rate (cpm),Counts,Counts uncertainty,Counts uncertainty (%)
+1,1,1,83.97,140,1.0,100,2023-11-30 08:44:20,100.0,139.95,11.830046491878212,8.453052155682895,1,2,1,252623.23,374237,1.125,100,2023-11-30 08:47:44,88.88888888888889,374256.63703703706,611.7651812885701,0.16346141143464313,0 days 00:00:00,0.0,252539.26,374116.68703703705,611.8795527201714,0.16355313032577887
+1,1,2,87.57,146,1.0,100,2023-11-30 08:51:04,100.0,145.95,12.080976781701056,8.277476383488219,1,2,2,251953.09,373593,1.124,100,2023-11-30 08:54:28,88.9679715302491,373595.9223013048,611.2249359289139,0.1636058906006906,0 days 00:06:44,404.0,251865.52,373449.9723013048,611.3443156694146,0.16370179703110896
+2,1,1,97.77,163,1.0,100,2023-12-01 12:46:16,100.0,162.95,12.765187033490735,7.833806096036044,2,2,1,223744.1,335987,1.11,100,2023-12-01 12:49:40,90.09009009009009,335952.1021021021,579.6137525129145,0.1725286875379512,1 days 04:01:56,100916.0,223646.33000000002,335789.15210210206,579.7543032199951,0.17265426818901866
+2,1,2,85.17,142,1.0,100,2023-12-01 12:53:00,100.0,141.95,11.914277149705725,8.393291405217138,2,2,2,223689.4,335843,1.11,100,2023-12-01 12:56:24,90.09009009009009,335869.96996996994,579.5428974372561,0.17254978094322423,1 days 04:08:40,101320.0,223604.22999999998,335728.01996996993,579.6653517073191,0.17265921139354673
+"""
+
 
 class TestHidexTDCRProcessor:
     @pytest.fixture
@@ -15,64 +61,40 @@ class TestHidexTDCRProcessor:
 
     @pytest.fixture
     def expected_summary(self):
-        csv_string = """Cicle,Repetitions,Real time (s),Date
-        1,2,100,2023-11-30 08:44:20
-        2,2,100,2023-12-01 12:46:16
-        """
-        df = pd.read_csv(StringIO(csv_string))
+        df = pd.read_csv(StringIO(summary))
         df[df.columns[-1]] = pd.to_datetime(df[df.columns[-1]], format='%Y-%m-%d %H:%M:%S')
         return df
 
     @pytest.fixture
     def expected_readings(self):
-        csv_string = """Cicle,Sample,Repetitions,Count rate (cpm),Counts (reading),Dead time (s),Real time (s),End time
-        1,1,1,83.97,140,1.0,100,2023-11-30 08:44:20
-        1,2,1,252623.23,374237,1.125,100,2023-11-30 08:47:44
-        1,1,2,87.57,146,1.0,100,2023-11-30 08:51:04
-        1,2,2,251953.09,373593,1.124,100,2023-11-30 08:54:28
-        2,1,1,97.77,163,1.0,100,2023-12-01 12:46:16
-        2,2,1,223744.1,335987,1.11,100,2023-12-01 12:49:40
-        2,1,2,85.17,142,1.0,100,2023-12-01 12:53:00
-        2,2,2,223689.4,335843,1.11,100,2023-12-01 12:56:24
-        """
-        df = pd.read_csv(StringIO(csv_string))
+        df = pd.read_csv(StringIO(readings))
         df[df.columns[-1]] = pd.to_datetime(df[df.columns[-1]], format='%Y-%m-%d %H:%M:%S')
         return df
 
     @pytest.fixture
     def expected_background(self):
-        csv_string = """Cicle,Sample,Repetitions,Count rate (cpm),Counts (reading),Dead time (s),Real time (s),End time,Live time (s),Counts,Counts uncertainty,Counts uncertainty (%)
-        1,1,1,83.97,140,1.0,100,2023-11-30 08:44:20,99.0,139.95,11.830046491878212,8.453052155682895
-        1,1,2,87.57,146,1.0,100,2023-11-30 08:51:04,99.0,145.95,12.080976781701056,8.277476383488219
-        2,1,1,97.77,163,1.0,100,2023-12-01 12:46:16,99.0,162.95,12.765187033490735,7.833806096036044
-        2,1,2,85.17,142,1.0,100,2023-12-01 12:53:00,99.0,141.95,11.914277149705725,8.393291405217138
-        """
-        df = pd.read_csv(StringIO(csv_string))
+        df = pd.read_csv(StringIO(background))
         df[df.columns[7]] = pd.to_datetime(df[df.columns[7]], format='%Y-%m-%d %H:%M:%S')
         return df
 
     @pytest.fixture
     def expected_sample(self):
-        csv_string = """Cicle,Sample,Repetitions,Count rate (cpm),Counts (reading),Dead time (s),Real time (s),End time,Live time (s),Counts,Counts uncertainty,Counts uncertainty (%)
-        1,2,1,252623.23,374237,1.125,100,2023-11-30 08:47:44,98.875,421038.7166666667,648.8749622744483,0.15411289665034722
-        1,2,2,251953.09,373593,1.124,100,2023-11-30 08:54:28,98.876,419921.81666666665,648.0137472821597,0.15431771381303394
-        2,2,1,223744.1,335987,1.11,100,2023-12-01 12:49:40,98.89,372906.8333333333,610.6609806867746,0.16375698327333088
-        2,2,2,223689.4,335843,1.11,100,2023-12-01 12:56:24,98.89,372815.6666666667,610.5863302323977,0.1637770042476034
-        """
-        df = pd.read_csv(StringIO(csv_string))
+        df = pd.read_csv(StringIO(sample))
         df[df.columns[7]] = pd.to_datetime(df[df.columns[7]], format='%Y-%m-%d %H:%M:%S')
         return df
 
     @pytest.fixture
     def expected_net(self):
-        csv_string = """Elapsed time,Elapsed time (s),Count rate (cpm),Counts,Counts uncertainty,Counts uncertainty (%)
-        0 days 00:00:00,0.0,252539.26,420898.76666666666,648.9827938140322,0.15418975896596013
-        0 days 00:06:44,404.0,251865.52,419775.86666666664,648.1263508504084,0.15439819253951279
-        1 days 04:01:56,100916.0,223646.33000000002,372743.8833333333,610.7943871167557,0.16386436221423956
-        1 days 04:08:40,101320.0,223604.22999999998,372673.7166666667,610.7025598985701,0.16387057433535226
-        """
-        df = pd.read_csv(StringIO(csv_string))
-        df[df.columns[0]] = pd.to_timedelta(df[df.columns[-0]])
+        df = pd.read_csv(StringIO(net))
+        df[df.columns[0]] = pd.to_timedelta(df[df.columns[0]])
+        return df
+
+    @pytest.fixture
+    def expected_compilation(self):
+        df = pd.read_csv(StringIO(compilation), header=[0, 1])
+        df[df.columns[7]] = pd.to_datetime(df[df.columns[7]], format='%Y-%m-%d %H:%M:%S')
+        df[df.columns[19]] = pd.to_datetime(df[df.columns[19]], format='%Y-%m-%d %H:%M:%S')
+        df[df.columns[24]] = pd.to_timedelta(df[df.columns[24]])
         return df
 
     def test_radionuclide(self, processor):
@@ -113,3 +135,6 @@ class TestHidexTDCRProcessor:
 
     def test_net(self, processor, expected_net):
         pd.testing.assert_frame_equal(processor.net, expected_net)
+
+    def test_compilation(self, processor, expected_compilation):
+        pd.testing.assert_frame_equal(processor.concatenate_results(), expected_compilation)
