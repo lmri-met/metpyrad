a1d8e02b55b0c5944efa3b05d67154273030ed42
Xandra Campo <xkmpera@gmail.com>
2025-01-31 13:37:35 +0100

    Script to reproducce the tutorials of the HidexTDCRProcessor class

---
+++ dev/doc_scripts/hidex_tdcr.py
@@ -0,0 +1,22 @@
+# Getting started
+import pandas as pd
+
+from metpyrad import HidexTDCRProcessor
+
+pd.set_option('display.max_columns', None)
+
+# Initialize the processor
+processor = HidexTDCRProcessor(radionuclide='Lu-177', year=2023, month=11)
+print(processor)
+
+# Path to the folder containing the CSV files
+folder_path = '../hidex_tdcr/test_case/input_files'
+
+# Parse the readings
+processor.parse_readings(folder_path)
+
+# Inspect the parsed readings
+print(processor.readings)
+
+# Print the summary of the measurements
+print(processor)
