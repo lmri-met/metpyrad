c5c8d90944799d916681699570577b9a8baedc0f
Xandra Campo <xkmpera@gmail.com>
2025-02-07 09:57:19 +0100

    Update TODOs

---
+++ dev/todos.py
@@ -0,0 +1,17 @@
+# Documentation
+# TODO: update module docstring
+# TODO: update class docstring TODO: add how tos and topics section
+# Deploy on GitHubPages
+
+# Test case
+# TODO: update test case with four files: script to get the string dataframes, string dataframes in tests, validation excel, examples in docstring and documentation
+# TODO: update scripts that generates the data for tests and docs
+
+# Testing
+# TODO: complete testing
+
+# Packaging
+# Publish first version
+
+# Source
+# Should HidexTDCR methods return the dataframe aside for assigning the attributes?
